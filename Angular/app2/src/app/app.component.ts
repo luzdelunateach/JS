7bac8ed441e6c8a5946c0b9361f9ad8f232cf5cc
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-05-17 14:11:34 -0500

    Ejercicios de jquery 5/17/2022

---
+++ Angular/app2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'My second angular app';
+  developer = 'Luz de luna Gutierrez';
+}
